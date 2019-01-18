5ef3927c051c7e6d4c3dd1b646d7c8907ed91d9a
MikeSotnichek <m.sotnichek@gmail.com>
2019-01-18 18:38:52 +0200

    Added mocks for some container

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -16,15 +16,41 @@
  *  1 registred object
  *  2 unregistred object
 */
+using IObject = void;
+
+class IKeyValueStore {
+public:
+    virtual ~IKeyValueStore() {}
+    virtual void Insert(int key, IObject* value) = 0;
+    virtual IObject* Query(int key) = 0;
+    virtual void Remove(int key) = 0;
+};
+
+class IKeyValueStoreFactory {
+public:
+    virtual ~IKeyValueStoreFactory() {}
+    virtual IKeyValueStore* CreateStore() = 0;
+};
+
+class MockKeyValueStoreFactory : public IKeyValueStoreFactory {
+public:
+    MOCK_METHOD0(CreateStore, IKeyValueStore*());
+};
+
+class MockKeyValueStore {
+public:
+    MOCK_METHOD2(Insert, void(int, IObject*));
+    MOCK_METHOD1(Query, IObject*(int));
+    MOCK_METHOD1(Remove, void(int));
+};
 
 TEST(SomeContainer, RegistersObjects) {
-    IKeyValueStoreFactory<MockKeyValueStore> mockStorageProvider;
+    MockKeyValueStoreFactory mockStorageProvider;
     MockKeyValueStore mockStore;
-    EXPECT_CALL(mockStorageProvider, CreateStore()).WillOnce(::testing::Return(mockStore));
-    CSomeContainer container(mockStorageProvider);
+    EXPECT_CALL(mockStorageProvider, CreateStore()).WillOnce(::testing::Return(&mockStore));
+    CSomeContainer container(&mockStorageProvider);
     int someIndex = 0;
-    int* someObject = new int(5);
-    EXPECT_CALL(mockStore, insert(someIndex, someObject));
+    std::auto_ptr<IObject> someObject(new int(5));
+    EXPECT_CALL(mockStore, Insert(someIndex, someObject.get()));
     container.Register(someIndex, someObject);
-
 }
