52a51eed61495abc43d7f7c833a0298526ca2f58
MikeSotnichek <m.sotnichek@gmail.com>
2019-01-18 18:39:54 +0200

    Refactoring - removed unnecessary mocks

--- src/mylib/SomeContainer.h
+++ src/mylib/SomeContainer.h
@@ -3,63 +3,35 @@
 #include <map>
 #include <cassert>
 
-template<typename IObject>
-class IKeyValueStoreIterator {
-public:
-    virtual ~IKeyValueStoreIterator() {}
-    virtual bool Next() = 0;
-    virtual IObject* Value() = 0;
-};
-
-template<typename IObject>
-class IKeyValueStore {
-public:
-    virtual ~IKeyValueStore() {}
-    virtual void Insert(int key, IObject* value) = 0;
-    virtual IObject* Query(int key) = 0;
-    virtual void Remove(int key) = 0;
-    virtual IKeyValueStoreIterator<IObject>& Begin() = 0;
-};
-
-template<typename IObject>
-class IKeyValueStoreFactory {
-public:
-    virtual ~IKeyValueStoreFactory() {}
-    virtual IKeyValueStore<IObject>* CreateStore() = 0;
-};
+template<typename KeyType, typename ValueType>
+using  KeyValueStore = std::map<KeyType, ValueType>;
 
 template<typename IObject>
 class CSomeContainer {
 public:
-    CSomeContainer(IKeyValueStoreFactory<IObject>* storageProvider);
+    CSomeContainer();
     ~CSomeContainer();
     void Register(int objectId, std::auto_ptr<IObject> object);
     IObject* Query(int objectId);
-    void Release(int objectId);
+    void Unregister(int objectId);
 private:
-    std::unique_ptr<IKeyValueStore<IObject>> m_storage;
+    KeyValueStore<int, IObject*> m_storage;
 };
 
 template<typename IObject>
-CSomeContainer<IObject>::CSomeContainer(IKeyValueStoreFactory<IObject>* storageProvider)
+CSomeContainer<IObject>::CSomeContainer()
 {
-    assert(storageProvider != nullptr && "storage provider must be non null");
-    if (storageProvider != nullptr) {
-        m_storage.reset(storageProvider->CreateStore());
-    }
 }
 
 template<typename IObject>
 CSomeContainer<IObject>::~CSomeContainer()
 {
-    try {
-        IKeyValueStoreIterator<IObject>& iterator = m_storage->Begin();
-        do {
-            IObject* val = iterator.Value();
-            if (val != nullptr) {
-                delete val;
-            }
-        } while(iterator.Next());
+    try
+    {
+        for (auto it = m_storage.begin(); it != m_storage.end(); ++it)
+        {
+            delete it->second;
+        }
     } catch (const std::exception &) {
         //
     }
@@ -68,19 +40,19 @@ CSomeContainer<IObject>::~CSomeContainer()
 template<typename IObject>
 void CSomeContainer<IObject>::Register(int objectId, std::auto_ptr<IObject> object)
 {
-    m_storage->Insert(objectId, object.release());
+    m_storage[objectId] = object.release();
 }
 
 template<typename IObject>
 IObject* CSomeContainer<IObject>::Query(int objectId)
 {
-    return m_storage->Query(objectId);
+    return m_storage.at(objectId);
 }
 
 template<typename IObject>
-void CSomeContainer<IObject>::Release(int objectId)
+void CSomeContainer<IObject>::Unregister(int objectId)
 {
-    IObject* objPtr = m_storage->Query(objectId);
-    m_storage->Remove(objectId);
+    IObject* objPtr = m_storage.at(objectId);
+    m_storage.erase(objectId);
     delete objPtr;
 }
--- src/test/test.cpp
+++ src/test/test.cpp
@@ -18,30 +18,6 @@
  *  2 unregistred object
 */
 
-template<typename IObject>
-class MockKeyValueStoreFactory : public IKeyValueStoreFactory<IObject> {
-public:
-    MOCK_METHOD0_T(CreateStore, IKeyValueStore<IObject>*());
-};
-
-template<typename IObject>
-class MockKeyValueStoreIterator : public IKeyValueStoreIterator<IObject> {
-public:
-    MOCK_METHOD0_T(Next, bool());
-    MOCK_METHOD0_T(Value, IObject*());
-};
-
-template<typename IObject>
-class MockKeyValueStore : public IKeyValueStore<IObject> {
-public:
-    MOCK_METHOD2_T(Insert, void(int, IObject*));
-    MOCK_METHOD1_T(Query, IObject*(int));
-    MOCK_METHOD1_T(Remove, void(int));
-    MOCK_METHOD0_T(Begin, IKeyValueStoreIterator<IObject>&());
-    MOCK_METHOD0(Die, void());
-    virtual ~MockKeyValueStore() { Die(); }
-};
-
 class IObjectDestructable {
 public:
     virtual ~IObjectDestructable() {}
@@ -53,77 +29,40 @@ public:
     virtual ~MockIObjectDestructable() { Die(); }
 };
 
-TEST(SomeContainer, UsesStorageFactory) {
-    MockKeyValueStoreFactory<int> mockStorageProvider;
-    MockKeyValueStore<int>* mockStore = new MockKeyValueStore<int>();
-    EXPECT_CALL(mockStorageProvider, CreateStore()).WillOnce(::testing::Return(mockStore));
-    CSomeContainer<int> container(&mockStorageProvider);
-}
-
 TEST(SomeContainer, RegistersObjects) {
-    MockKeyValueStoreFactory<int> mockStorageProvider;
-    MockKeyValueStore<int>* mockStore = new MockKeyValueStore<int>();
-    EXPECT_CALL(mockStorageProvider, CreateStore()).WillOnce(::testing::Return(mockStore));
-    CSomeContainer<int> container(&mockStorageProvider);
-
+    CSomeContainer<int> container;
     int someIndex = 0;
     std::auto_ptr<int> someObject(new int(5));
-    EXPECT_CALL(*mockStore, Insert(someIndex, someObject.get()));
     container.Register(someIndex, someObject);
 }
 
 TEST(SomeContainer, QueryObjects) {
-    MockKeyValueStoreFactory<int> mockStorageProvider;
-    MockKeyValueStore<int>* mockStore = new MockKeyValueStore<int>();
-    EXPECT_CALL(mockStorageProvider, CreateStore()).WillOnce(::testing::Return(mockStore));
-    CSomeContainer<int> container(&mockStorageProvider);
-
+    CSomeContainer<int> container;
     int someIndex = 0;
     int expectedValue = 5;
     int* storedObject = new int(expectedValue);
     std::auto_ptr<int> someObject(storedObject);
-    EXPECT_CALL(*mockStore, Insert(someIndex, someObject.get()));
     container.Register(someIndex, someObject);
-
-    EXPECT_CALL(*mockStore, Query(someIndex)).WillOnce(::testing::Return(storedObject));
-
     EXPECT_EQ(expectedValue, *container.Query(someIndex));
 }
 
 TEST(SomeContainer, ReleaseObjects) {
-    MockKeyValueStoreFactory<int> mockStorageProvider;
-    MockKeyValueStore<int>* mockStore = new MockKeyValueStore<int>();
-    EXPECT_CALL(mockStorageProvider, CreateStore()).WillOnce(::testing::Return(mockStore));
-    CSomeContainer<int> container(&mockStorageProvider);
-
+    CSomeContainer<int> container;
     int someIndex = 0;
     int expectedValue = 5;
     int* storedObject = new int(expectedValue);
     std::auto_ptr<int> someObject(storedObject);
-    EXPECT_CALL(*mockStore, Insert(someIndex, someObject.get()));
     container.Register(someIndex, someObject);
-
-    EXPECT_CALL(*mockStore, Query(someIndex)).WillOnce(::testing::Return(storedObject));
-    EXPECT_CALL(*mockStore, Remove(someIndex));
-    container.Release(someIndex);
+    EXPECT_EQ(expectedValue, *container.Query(someIndex));
+    container.Unregister(someIndex);
+    EXPECT_THROW(container.Query(someIndex), std::out_of_range);
 }
 
 TEST(SomeContainer, DestroysObjectsInStore) {
-    MockKeyValueStoreFactory<IObjectDestructable> mockStorageProvider;
-    MockKeyValueStore<IObjectDestructable>* mockStore = new MockKeyValueStore<IObjectDestructable>();
-    MockKeyValueStoreIterator<IObjectDestructable> mockStoreIterator;
-    EXPECT_CALL(mockStorageProvider, CreateStore()).WillOnce(::testing::Return(mockStore));
-    CSomeContainer<IObjectDestructable>* container = new CSomeContainer<IObjectDestructable>(&mockStorageProvider);
-
+    CSomeContainer<IObjectDestructable> container;
     int someIndex = 0;
     MockIObjectDestructable* storedObject = new MockIObjectDestructable;
     std::auto_ptr<IObjectDestructable> someObject(storedObject);
-    EXPECT_CALL(*mockStore, Insert(someIndex, someObject.get()));
-    container->Register(someIndex, someObject);
-    EXPECT_CALL(*mockStore, Begin()).WillOnce(::testing::ReturnRef(mockStoreIterator));
-    EXPECT_CALL(mockStoreIterator, Value()).WillOnce(::testing::Return(storedObject));
-    EXPECT_CALL(mockStoreIterator, Next()).WillOnce(::testing::Return(false));
+    container.Register(someIndex, someObject);
     EXPECT_CALL(*storedObject, Die());
-    EXPECT_CALL(*mockStore, Die());
-    delete container;
 }
